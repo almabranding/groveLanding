443f999e5fd2a2403f7eec28a02be9149616e868
dan <dan@almabranding.com>
2013-04-26 12:28:47 +0200

    26 Abril 2013 Implantación del scrolling horizontal.

--- views/gallery/index.php
+++ views/gallery/index.php
@@ -3,7 +3,6 @@
 </script>
 
 <?php for($i=0;$i<20;$i++){?>
-
 <div class="bgContainer">
     <div class="body-background" class="">
         <img class="imgBG" src="<?php echo URL . 'public/images/home.jpg'; ?>" alt="Bg">
@@ -12,6 +11,5 @@
         Descripcion
     </div>
 </div>
-
 <?php }?>
 
